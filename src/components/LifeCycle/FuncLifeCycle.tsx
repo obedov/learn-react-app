5661bb7ee2567b9f851227ba440742cca0e66d02
Nikita Obedkov <nikita.obyedkov@gmail.com>
2022-11-07 22:38:12 +0300

    Add func life cycle

---
+++ src/components/LifeCycle/FuncLifeCycle.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { useEffect, useLayoutEffect, useContext, useReducer, useRef } from 'react';
+
+export function FuncLifeCycle() {
+    useEffect(() => console.count('useEffect'), []);
+
+    useLayoutEffect(() => console.count('useLayoutEffect'), []);
+
+    return <>{console.count('Rendering JSX')}</>;
+}
